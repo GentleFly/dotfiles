3374106c21b06d307dbe50fbd08cceb5bec1ed8f
GentleFly <muhamatnabeev@yandex.ru>
2017-12-15 23:33:51 +0300

    Added command for list all files in ".DotFile" and added function "export color for bash"

--- dotfiles.sh
+++ dotfiles.sh
@@ -11,9 +11,86 @@ _dotfiles_sys_home_dir=~/
 _dotfiles_sys_root_dir=/
 
 _dotfiles_commands_inc="include reinclude exclude"
-_dotfiles_commands_set="setup setup_all try_setup try_setup_all unsetup unsetup_all"
+_dotfiles_commands_set="setup setup_all try_setup try_setup_all unsetup unsetup_all list"
 _dotfiles_commands_help="help"
 _dotfiles_commands="${_dotfiles_commands_inc} ${_dotfiles_commands_set} ${_dotfiles_commands_help}"
+
+function _dotfiles_export_colors() { #{{{
+# Reference: https://wiki.archlinux.org/index.php/Bash/Prompt_customization_(%D0%A0%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9)
+
+	# Сброс
+	Color_Off='\e[0m'       # Text Reset
+
+	# Обычные цвета
+	Black='\e[0;30m'        # Black
+	Red='\e[0;31m'          # Red
+	Green='\e[0;32m'        # Green
+	Yellow='\e[0;33m'       # Yellow
+	Blue='\e[0;34m'         # Blue
+	Purple='\e[0;35m'       # Purple
+	Cyan='\e[0;36m'         # Cyan
+	White='\e[0;37m'        # White
+
+	# Жирные
+	BBlack='\e[1;30m'       # Black
+	BRed='\e[1;31m'         # Red
+	BGreen='\e[1;32m'       # Green
+	BYellow='\e[1;33m'      # Yellow
+	BBlue='\e[1;34m'        # Blue
+	BPurple='\e[1;35m'      # Purple
+	BCyan='\e[1;36m'        # Cyan
+	BWhite='\e[1;37m'       # White
+
+	# # Подчёркнутые
+	# UBlack='\e[4;30m'       # Black
+	# URed='\e[4;31m'         # Red
+	# UGreen='\e[4;32m'       # Green
+	# UYellow='\e[4;33m'      # Yellow
+	# UBlue='\e[4;34m'        # Blue
+	# UPurple='\e[4;35m'      # Purple
+	# UCyan='\e[4;36m'        # Cyan
+	# UWhite='\e[4;37m'       # White
+
+	# # Фоновые
+	# On_Black='\e[40m'       # Black
+	# On_Red='\e[41m'         # Red
+	# On_Green='\e[42m'       # Green
+	# On_Yellow='\e[43m'      # Yellow
+	# On_Blue='\e[44m'        # Blue
+	# On_Purple='\e[45m'      # Purple
+	# On_Cyan='\e[46m'        # Cyan
+	# On_White='\e[47m'       # White
+
+	# # Высоко Интенсивные
+	# IBlack='\e[0;90m'       # Black
+	# IRed='\e[0;91m'         # Red
+	# IGreen='\e[0;92m'       # Green
+	# IYellow='\e[0;93m'      # Yellow
+	# IBlue='\e[0;94m'        # Blue
+	# IPurple='\e[0;95m'      # Purple
+	# ICyan='\e[0;96m'        # Cyan
+	# IWhite='\e[0;97m'       # White
+
+	# # Жирные Высоко Интенсивные
+	# BIBlack='\e[1;90m'      # Black
+	# BIRed='\e[1;91m'        # Red
+	# BIGreen='\e[1;92m'      # Green
+	# BIYellow='\e[1;93m'     # Yellow
+	# BIBlue='\e[1;94m'       # Blue
+	# BIPurple='\e[1;95m'     # Purple
+	# BICyan='\e[1;96m'       # Cyan
+	# BIWhite='\e[1;97m'      # White
+
+	# # Высоко Интенсивные фоновые
+	# On_IBlack='\e[0;100m'   # Black
+	# On_IRed='\e[0;101m'     # Red
+	# On_IGreen='\e[0;102m'   # Green
+	# On_IYellow='\e[0;103m'  # Yellow
+	# On_IBlue='\e[0;104m'    # Blue
+	# On_IPurple='\e[0;105m'  # Purple
+	# On_ICyan='\e[0;106m'    # Cyan
+	# On_IWhite='\e[0;107m'   # White
+} #}}}
 
 _dotfiles_convert_path_dotfile_to_system_for_file() { #{{{
     # _dotfiles_convert_path_dotfile_to_system_for_file home/.vimrc
@@ -511,6 +588,52 @@ _dotfiles_exclude() { #{{{
     return 0
 } #}}}
 
+_dotfiles_check_installed() { #{{{
+    if [ $# -ne 1 ] ; then
+        return 1
+    fi
+
+    local source_file=$1
+    local symlink=$(_dotfiles_convert_path_dotfile_to_system_for_file "${source_file}")
+    echo -n "${source_file} <- "
+    if [ -L ${symlink} ]; then
+        echo -e "${Green}${symlink}${Color_Off}"
+    else
+        echo -e "${BRed}file not setup ${Red}(symlink not exist)!${Color_Off}"
+    fi
+
+    return 0
+} #}}}
+
+_dotfiles_list() { #{{{
+
+    if [ $# == 0 ] ; then
+        if [ -d ${_dotfiles_home_dir} ] ; then
+            for source_file in $(find ${_dotfiles_home_dir} -type f)
+            do
+                _dotfiles_check_installed ${source_file}
+            done
+        fi
+        if [ -d ${_dotfiles_root_dir} ] ; then
+            for source_file in $(find ${_dotfiles_root_dir} -type f)
+            do
+                _dotfiles_check_installed ${source_file}
+            done
+        fi
+    else
+        while ((${#})); do
+            if [ -f ${1} ] ; then
+                _dotfiles_check_installed ${1}
+            else
+                echo -e "${1} ${BRed}file not exist or not setup"
+            fi
+            shift
+        done
+    fi
+
+    return 0
+} #}}}
+
 _dotfiles_help() {  #{{{
     echo "usage:"
     echo ""
@@ -553,6 +676,8 @@ _dotfiles_help() {  #{{{
 # TODO:
 # source lib.sh
 dotfiles() { #{{{
+
+	_dotfiles_export_colors
 
     if [ $# == 0 ] ; then
         _dotfiles_help
@@ -631,4 +756,3 @@ _dotfiles_completion() { #{{{
 complete -F _dotfiles_completion dotfiles
 
 # vim:set foldmethod=marker:
-
